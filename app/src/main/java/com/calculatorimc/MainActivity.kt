107b40b8cac05e8b10169e0ac9688b644d700ad7
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-04-27 16:45:59 -0400

    Criação do layout de calculo

--- app/src/main/java/com/calculatorimc/MainActivity.kt
+++ app/src/main/java/com/calculatorimc/MainActivity.kt
@@ -2,10 +2,25 @@ package com.calculatorimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.calculatorimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+        binding.btnCalcular.setOnClickListener {
+        }
+
     }
+
+    fun view(){
+    }
+
 }
